f9230de3fe16842e0dd1cb7b7fde3d2cbb3ee8ed
Iwannabegosu <winlslouder3@gmail.com>
2022-11-28 15:56:10 +0900

    #4 새 버튼 추가

--- calculator_main.py
+++ calculator_main.py
@@ -14,7 +14,7 @@ class Main(QDialog):
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        self.equation_solution = QLineEdit("")
+        self.equation_solution = QLineEdit("0")
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
         layout_equation_solution.addRow(self.equation_solution)
@@ -33,10 +33,10 @@ class Main(QDialog):
         button_division.clicked.connect(lambda state, operation = "/": self.button_operation_clicked(operation))
 
         ### 사칙연산 버튼을 layout_operation 레이아웃에 추가s
-        layout_button.addWidget(button_plus, 3, 3)
-        layout_button.addWidget(button_minus,2, 3)
-        layout_button.addWidget(button_product, 1, 3)
-        layout_button.addWidget(button_division, 0, 3)
+        layout_button.addWidget(button_plus, 4, 3)
+        layout_button.addWidget(button_minus,3, 3)
+        layout_button.addWidget(button_product, 2, 3)
+        layout_button.addWidget(button_division, 1, 3)
 
         ### =, clear, backspace 버튼 생성
         button_equal = QPushButton("=")
@@ -49,9 +49,9 @@ class Main(QDialog):
         button_backspace.clicked.connect(self.button_backspace_clicked)
 
         ### =, clear, backspace 버튼을 layout_clear_equal 레이아웃에 추가
-        layout_button.addWidget(button_clear, 0, 0)
-        layout_button.addWidget(button_backspace, 0, 1)
-        layout_button.addWidget(button_equal, 4, 3)
+       #layout_button.addWidget(button_clear, 0, 0)
+        layout_button.addWidget(button_backspace, 0, 3)
+        layout_button.addWidget(button_equal, 5, 3)
 
         ### 숫자 버튼 생성하고, layout_number 레이아웃에 추가
         ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
@@ -64,24 +64,41 @@ class Main(QDialog):
                 x,y = divmod(number-1, 3)
                 if(x == 0):
                     x += 2
-                    layout_button.addWidget(number_button_dict[number], x+1, y)
+                    layout_button.addWidget(number_button_dict[number], x + 2, y)
                 elif(x == 1):
-                    layout_button.addWidget(number_button_dict[number], x + 1, y)
+                    layout_button.addWidget(number_button_dict[number], x + 2, y)
                 elif(x == 2):
                     x = 0
-                    layout_button.addWidget(number_button_dict[number], x + 1, y)
+                    layout_button.addWidget(number_button_dict[number], x + 2, y)
 
             elif number==0:
-                layout_button.addWidget(number_button_dict[number], 4, 1)
+                layout_button.addWidget(number_button_dict[number], 5, 1)
 
         ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
         button_dot = QPushButton(".")
         button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
-        layout_button.addWidget(button_dot, 4, 2)
+        layout_button.addWidget(button_dot, 5, 2)
 
         button_double_zero = QPushButton("00")
         button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
-        layout_button.addWidget(button_double_zero, 4, 0)
+        layout_button.addWidget(button_double_zero, 5, 0)
+
+        # % CE C 1/x x^2 제곱근
+        button_remainder = QPushButton("%")
+        button_CE = QPushButton("CE")
+        button_C = QPushButton("C")
+        button_oneperx = QPushButton("1/x")
+        button_square = QPushButton("x²")
+        button_root = QPushButton("√x")
+
+        # 새 버튼 그리드에 추가
+        layout_button.addWidget(button_remainder, 0, 0)
+        layout_button.addWidget(button_CE, 0, 1)
+        layout_button.addWidget(button_C, 0, 2)
+        layout_button.addWidget(button_oneperx, 1, 0)
+        layout_button.addWidget(button_square, 1, 1)
+        layout_button.addWidget(button_root, 1, 2)
+
 
         ### 각 레이아웃을 main_layout 레이아웃에 추가
         main_layout.addLayout(layout_equation_solution)
@@ -94,6 +111,10 @@ class Main(QDialog):
     ### functions ###
     #################
     def number_button_clicked(self, num):
+        # 0 이면 지움
+        if self.equation_solution.text() == '0':
+            self.equation_solution.setText("")
+
         equation = self.equation_solution.text()
         equation += str(num)
         self.equation_solution.setText(equation)
